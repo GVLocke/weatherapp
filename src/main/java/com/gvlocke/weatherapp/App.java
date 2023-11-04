b179e0e2ab15b054e207aa0eb7a2f7d5a6af3009
G Locke <jkeys726@gmail.com>
2023-11-03 19:46:47 -0500

    Deleted unused `App.java`

--- src/main/java/com/gvlocke/weatherapp/App.java
+++
@@ -1,9 +0,0 @@
-package com.gvlocke.weatherapp;
-
-public class App {
-    public static void main(String[] args) {
-        Forcastor forcastor = new Forcastor();
-        Weather weather = forcastor.getWeather();
-        System.out.println(weather);
-    }
-}
